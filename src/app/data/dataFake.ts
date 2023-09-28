542627c019decacb487ced92bdde684f2fbdea21
Sdavi3489 <sdavi3489@gmail.com>
2023-09-28 19:18:42 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,33 @@
+/*
+o fataFake vai ser um vetor
+como se fosse um banco de dados fictpicio
+Vamos utilizar para pegar os dados dinamicamente
+
+*/
+
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Novo homem de ferro em 3D",
+        "description": "MARVEL ANUNCIA NOVO FILME DO HOMEM DE FERRO, CONFIRA!",
+        "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+    {
+        "id": "2",
+        "title": "Nova série anunciada no Disney+",
+        "description": "Novas séries estão por vir no Disney+, saiba quais são as novidades!",
+        "photoCover": "https://br.web.img2.acsta.net/r_1280_720/newsv7/19/04/22/20/34/5550692.jpg"
+    },   
+    {
+        "id": "3",
+        "title": "Novo filme do pantera negra lançado em breve",
+        "description": "Novo filme do Pantera Negra será lançado em breve nos cinemas!",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRXWGyKOjzA0Mu_tmlXvX2uuHzIp7d3A08qQ&usqp=CAU"
+    }, 
+    {
+        "id": "4",
+        "title": "Novo filme dos x-men está pós produção, afirma diretor",
+        "description": "Diretor afirma a volta dos x-men!",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeRkyr9_hVvSVtpXh1gdrwfoqanthJaPCHJg&usqp=CAU"
+    }
+]
